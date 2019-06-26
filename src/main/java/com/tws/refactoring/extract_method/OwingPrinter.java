5a5b1e9d95fabb8b73761176c88c8ee969b85782
xsu <xsu@thoughtworks.com>
2019-06-26 16:54:41 +0800

    extract methods for printer.

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -1,29 +1,36 @@
 package com.tws.refactoring.extract_method;
 
-import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Vector;
 
 public class OwingPrinter {
     void printOwing(String name, List<Order> orders) {
         Iterator<Order> elements = orders.iterator();
         double outstanding = 0.0;
+        printBanner();
 
-        // print banner
-        System.out.println ("*****************************");
-        System.out.println ("****** Customer totals ******");
-        System.out.println ("*****************************");
+        outstanding = printOwings(elements, outstanding);
 
-        // print owings
+        printDetails(name, outstanding);
+    }
+
+    private void printDetails(String name, double outstanding) {
+        System.out.println("name: " + name);
+        System.out.println("amount: " + outstanding);
+    }
+
+    private double printOwings(Iterator<Order> elements, double outstanding) {
         while (elements.hasNext()) {
             Order each = (Order) elements.next();
             outstanding += each.getAmount();
         }
+        return outstanding;
+    }
 
-        // print details
-        System.out.println("name: " + name);
-        System.out.println("amount: " + outstanding);
+    private void printBanner() {
+        System.out.println ("*****************************");
+        System.out.println ("****** Customer totals ******");
+        System.out.println ("*****************************");
     }
 }
 

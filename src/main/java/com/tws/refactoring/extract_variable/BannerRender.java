0f3452463e075c89f745f7195535c98cf60bfb62
xsu <xsu@thoughtworks.com>
2019-06-28 12:33:28 +0800

    extract variable for banner.

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,8 +2,9 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().contains("MAC")) &&
-                (browser.toUpperCase().contains("IE"))) {
+        boolean isMac = platform.toUpperCase().contains("MAC");
+        boolean isIE = browser.toUpperCase().contains("IE");
+        if (isMac && isIE) {
             return "IE on Mac?";
         }
         return "banner";
